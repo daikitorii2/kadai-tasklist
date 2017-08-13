fca96eed94b1ab33505a3801dc3af7b7603be844
Daiki Torii <dai7707@hi-net.zaq.ne.jp>
2017-08-13 02:17:09 +0000

    Lesson9 課題：カラムの追加 【再提出】→マイグレーションファイル再作成

--- db/migrate/20170810072753_add_status_to_tasks.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusToTasks < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :tasks, :status, :string
-    add_column :tasks, :status, :string, limit:10
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810072753) do
+ActiveRecord::Schema.define(version: 20170812233645) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
